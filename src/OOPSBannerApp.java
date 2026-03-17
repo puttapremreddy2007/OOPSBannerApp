1871dc4f78676f9c85276d9020a46bde4fbb7e92
puttapremreddy2007 <puttapremreddy2007@gmail.com>
2026-03-17 11:52:01 +0300

    UC8

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,24 +1,79 @@
 /*
-*OOPSBannerApp UC5
+*OOPSBannerApp UC8
 *
 *@author Prem
-*@version 5.0 
+*@version 8.0 
 */
 
-public class OOPSBannerApp{
-    public static void main(String[] args) {
-    String[] lines = {
-        String.join(" ","   ***   " , " " , "   ***   " , " " , " ******   " , " " , "   *****     "),
-        String.join(" "," **   ** " , " " , " **   ** " , " " , " **    ** " , " " , "  **    **   "),
-        String.join(" "," **   ** " , " " , " **   ** " , " " , " **    ** " , " " , " **          "),
-        String.join(" "," **   ** " , " " , " **   ** " , " " , " ******   " , " " , "   *****     "),
-        String.join(" "," **   ** " , " " , " **   ** " , " " , " **       " , " " , "         **  "),
-        String.join(" "," **   ** " , " " , " **   ** " , " " , " **       " , " " , " **     **   "),
-        String.join(" ","   ***   " , " " , "   ***   " , " " , " **       " , " " , "   *****     ")
-    };
-    for(String line : lines){
-        System.out.println(line);
+import java.util.HashMap;
+
+public class OOPSBannerApp {
+
+    public static HashMap<Character, String[]> createCharacterMap() {
+        HashMap<Character, String[]> charMap = new HashMap<>();
+
+        charMap.put('O', new String[]{
+            "   ***   " ,
+            " **   ** " ,
+            " **   ** " ,
+            " **   ** " ,
+            " **   ** " ,
+            " **   ** " ,
+            "   ***   "
+        });
+
+        charMap.put('P', new String[]{
+            " ******   " ,
+            " **    ** " ,
+            " **    ** " ,
+            " ******   " ,
+            " **       " ,
+            " **       " ,
+            " **       " 
+        });
+
+        charMap.put('S', new String[]{
+            "   *****     " ,
+            "  **    **   " ,
+            " **          " ,
+            "   *****     " ,
+            "         **  " ,
+            " **     **   " ,
+            "   *****     "
+        });
+
+        charMap.put(' ', new String[]{
+            "    ",
+            "    ",
+            "    ",
+            "    ",
+            "    ",
+            "    ",
+            "    "
+        });
+
+        return charMap;
     }
-    
+
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+        int patternHeight = charMap.get('O').length;
+
+        for (int line = 0; line < patternHeight; line++) {
+            StringBuilder sb = new StringBuilder();
+            
+            for (char ch : message.toCharArray()) {
+                char upperCh = Character.toUpperCase(ch);
+                String[] pattern = charMap.getOrDefault(upperCh, charMap.get(' '));
+                sb.append(pattern[line]).append(" ");
+            }
+            
+            System.out.println(sb.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<Character, String[]> charMap = createCharacterMap();
+        String message = "OOPS";
+        displayBanner(message, charMap);
     }
 }
